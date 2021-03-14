12ced30ea78cb7d004f6092a4ce2b838a75283e9
dtrizna <ditrizna@protonmail.com>
2021-03-14 13:09:01 +0200

    reading labels & encoding errors

--- data.py
+++ data.py
@@ -12,7 +12,6 @@ import xml.etree.ElementTree as ET
 exclude = set(string.punctuation)
 stopwords_en = set(stopwords.words('english'))
 
-
 def clean_document(doc):
     clean_punc = ''.join(ch if ch not in exclude else '' for ch in doc.lower())
     clean_punc_tokens = word_tokenize(clean_punc)
@@ -20,40 +19,53 @@ def clean_document(doc):
     #clean_digits = [tok for tok in clean_stop if re.match(r'^([\s\d]+)$', tok) is None]
     return clean_stop
 
-
 def parse_reuters_articles(filepath):
     direc = os.listdir(filepath)
     direc = [dir for dir in direc if re.match(r'^([\s\d]+)$', dir) is not None]
     articles = []
     errors = []
     for dir in direc:
-        dir_path = filepath + dir
+        dir_path = filepath + dir if filepath.endswith("/") else filepath + "/" + dir
         xml_files = os.listdir(dir_path)
         for f in xml_files:
-            art_file = open(dir_path + "/" + f, 'r', encoding='utf-8')
-            print("Articles:", len(articles))
+            art_file = open(dir_path + "/" + f, 'r', encoding='ISO-8859-1')
+            print(" [*] Reading articles:", len(articles), end="\r")
             try:
                 tree = ET.parse(art_file)
                 article_dict = {}
                 root = tree.getroot()
                 article_dict['id'] = root.attrib['itemid']
                 article_dict['date'] = root.attrib['date']
+                article_dict['codes'] = []
+
                 for child in root:
                     if "headline" in child.tag:
-                        headline = child
-                        article_dict['headline'] = headline.text
+                        article_dict['headline'] = child.text
                     elif "text" in child.tag:
-                        text = child
                         article_text = ''
-                        for par in text:
+                        for par in child:
                             article_text += par.text + " "
                         article_dict['text'] = article_text
+                    # getting labels
+                    elif "metadata" in child.tag:
+                        for par in child:
+                            if "codes" == par.tag and "topics" in par.attrib["class"]:
+                                    article_dict['codes'] = [code.attrib["code"] for code in par]                    
                 articles.append(article_dict)
-            except:
+
+            except Exception as ex:
                 print("Error parsing file", dir_path+'/'+f)
+                print(ex)
                 errors.append(dir_path+'/'+f)
     return articles
 
+if __name__ == "__main__":
+    import sys
+    import time
+    now = time.time()
+    data = parse_reuters_articles(sys.argv[1])
+    print(f"\n [!] Took {time.time() - now:.2f}s")
+    import pdb; pdb.set_trace()
 
 # def get_reuters_articles(filepath, max_art=50000):
 #     data = pickle.load(open(filepath,"rb"))[:max_art]

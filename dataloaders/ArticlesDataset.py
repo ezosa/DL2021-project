c9c28444e2ce34009ecb8de28b0afbb6ce0e4b6f
Elaine Zosa <elaine.zosa@helsinki.fi>
2021-03-16 15:57:15 +0200

    fix bugs in dataloader

--- dataloaders/ArticlesDataset.py
+++ dataloaders/ArticlesDataset.py
@@ -16,7 +16,7 @@ class ArticlesDataset(Dataset):
         row = dict(self.data.iloc()[idx])
         combined_text = row['headline'] + ' ' + row['text']
         combined_text = combined_text.lower().split()
-        combined_text = [self.vocab(w) if w in self.vocab else self.vocab['OOV'] for w in combined_text]
+        combined_text = [self.vocab[w] if w in self.vocab else self.vocab['OOV'] for w in combined_text]
         if len(combined_text) > max_text_len:
                 combined_text = combined_text[:max_text_len]
         else:
@@ -25,7 +25,7 @@ class ArticlesDataset(Dataset):
         row['content_len'] = max_text_len
         codes = row['codes']
         codes = codes.split()
-        codes2id = np.array([self.label2id(code) if code in self.label2id for code in codes])
+        codes2id = np.array([self.label2id[code] for code in codes if code in self.label2id])
         binary_label = np.zeros(self.labels_len)
         binary_label[codes2id] = 1
         row['binary_label'] = binary_label
